0859ab0c528de7055148b9edae8afe793e30c536
Nick Anderson <nickanderson@gmail.com>
2019-04-19 07:14:28 -0700

    setup application

---
+++ src/Components/OrginalD3.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class OriginalD3 extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Example</h2>
+      </div>
+    )
+  }
+}
+
+export default OriginalD3
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import "./styles.css";
+import './styles.css'
+
+import OrginialD3 from './Components/OrginalD3'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <OrginialD3 />
     </div>
-  );
+  )
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
