1278c41dfbc2413a467b22081119da8ec8bbbe80
Nick Anderson <nickanderson@gmail.com>
2019-04-19 07:29:27 -0700

    recreation or d3 brush in react. has performance issues

--- src/Components/OrginalD3.js
+++ src/Components/OrginalD3.js
@@ -1,10 +1,153 @@
 import React, { Component } from 'react'
+import * as d3 from 'd3'
 
 class OriginalD3 extends Component {
+  // state = {
+  //   random: d3.randomNormal(0, 0.2),
+  //   sqrt3: Math.sqrt(3),
+  //   points0: d3.range(300).map(function() {
+  //     return [this.state.random() + sqrt3, this.state.random() + 1, 0]
+  //   })
+  // }
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.drawSvg()
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+    this.drawSvg()
+  }
+
+  drawSvg = () => {
+    const random = d3.randomNormal(0, 0.2)
+    const sqrt3 = Math.sqrt(3),
+      points0 = d3.range(300).map(function() {
+        return [random() + sqrt3, random() + 1, 0]
+      }),
+      points1 = d3.range(300).map(function() {
+        return [random() - sqrt3, random() + 1, 1]
+      }),
+      points2 = d3.range(300).map(function() {
+        return [random(), random() - 1, 2]
+      }),
+      points = d3.merge([points0, points1, points2])
+    const svg = d3.select('svg'),
+      width = +svg.attr('width'),
+      height = +svg.attr('height')
+
+    const k = height / width,
+      x0 = [-4.5, 4.5],
+      y0 = [-4.5 * k, 4.5 * k],
+      x = d3
+        .scaleLinear()
+        .domain(x0)
+        .range([0, width]),
+      y = d3
+        .scaleLinear()
+        .domain(y0)
+        .range([height, 0]),
+      z = d3.scaleOrdinal(d3.schemeCategory10)
+
+    const xAxis = d3.axisTop(x).ticks(12),
+      yAxis = d3.axisRight(y).ticks((12 * height) / width)
+
+    let brush = d3.brush().on('end', brushended),
+      idleTimeout,
+      idleDelay = 350
+
+    svg
+      .selectAll('circle')
+      .data(points)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) {
+        return x(d[0])
+      })
+      .attr('cy', function(d) {
+        return y(d[1])
+      })
+      .attr('r', 2.5)
+      .attr('fill', function(d) {
+        return z(d[2])
+      })
+
+    svg
+      .append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + (height - 10) + ')')
+      .call(xAxis)
+
+    svg
+      .append('g')
+      .attr('class', 'axis axis--y')
+      .attr('transform', 'translate(10,0)')
+      .call(yAxis)
+
+    svg.selectAll('.domain').style('display', 'none')
+
+    svg
+      .append('g')
+      .attr('class', 'brush')
+      .call(brush)
+
+    function brushended() {
+      const s = d3.event.selection
+      if (!s) {
+        if (!idleTimeout) return (idleTimeout = setTimeout(idled, idleDelay))
+        x.domain(x0)
+        y.domain(y0)
+      } else {
+        console.log(s)
+        console.log(x)
+        x.domain([s[0][0], s[1][0]].map(x.invert, x))
+        y.domain([s[1][1], s[0][1]].map(y.invert, y))
+        svg.select('.brush').call(brush.move, null)
+      }
+      zoom()
+    }
+
+    function idled() {
+      idleTimeout = null
+    }
+
+    function zoom() {
+      const t = svg.transition().duration(750)
+      svg
+        .select('.axis--x')
+        .transition(t)
+        .call(xAxis)
+      svg
+        .select('.axis--y')
+        .transition(t)
+        .call(yAxis)
+      const circle = svg.selectAll('circle')
+      console.log('circle: ', circle)
+      svg
+        .selectAll('circle')
+        .transition(t)
+        .attr('cx', d => {
+          console.log(d)
+          console.log(x)
+          let cx = x(d[0])
+          console.log(cx)
+          return cx
+        })
+        .attr('cy', d => {
+          console.log(d)
+          console.log(y)
+          let cy = y(d[1])
+          console.log(cy)
+          return cy
+        })
+    }
+  }
+
   render() {
     return (
       <div>
-        <h2>Example</h2>
+        <h2>Original D3</h2>
+        <svg width="960" height="600" />
       </div>
     )
   }
