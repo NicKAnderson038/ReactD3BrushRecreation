ec2c51445cae78eb3324d8cfb52a942b95600f09
Nick Anderson <andersonnick038@gmail.com>
2019-08-20 14:48:13 -0400

    data file single location

--- src/Components/LineGraphReact/GridD3Main.js
+++ src/Components/LineGraphReact/GridD3Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import * as d3 from 'd3'
 
-import { data } from './LineData'
+import { data } from '../../data/LineData'
 
 import { logColor } from '../../Helpers/consoleLogStyle'
 import GridLines from './GridLines'
